188b7e08a18e24407b5bf15ab77f3e5e29dada63
Raman Nag <raman.nag.ngit@gmail.com>
2026-05-30 12:38:44 +0530

    add users table with alembic migration

--- backend/alembic_old/env.py
+++ backend/alembic_old/env.py
@@ -1,11 +1,10 @@
 from logging.config import fileConfig
-
+from app.core.settings import settings
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.core.settings import settings
-from app.db.base import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -13,7 +12,6 @@ config.set_main_option(
     "sqlalchemy.url",
     settings.DATABASE_URL
 )
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -23,6 +21,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+from app.db.base import Base
+
 target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
